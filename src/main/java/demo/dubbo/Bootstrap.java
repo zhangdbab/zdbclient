b653c0eaa26a0c8eea7faa84eb2d74f2adb2b6d5
张东彬(DJ009828) <dj009828zhangdongbin@djbx.com>
2020-10-31 19:55:24 +0800

    dubbo+demo 不使用注册中心

---
+++ src/main/java/demo/dubbo/Bootstrap.java
@@ -0,0 +1,21 @@
+package demo.dubbo;
+
+import demo.dubbo.api.IHelloService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+/**
+ * Created by DJ009828 on 2020/10/31 18:56
+ */
+public class Bootstrap {
+    public static void main(String[] args) throws IOException {
+        ClassPathXmlApplicationContext classPathXmlApplicationContext
+                =new ClassPathXmlApplicationContext("dubbo-client.xml") ;
+        IHelloService iHelloService =(IHelloService)classPathXmlApplicationContext.getBean("iHelloService");
+//        iHelloService.helle("zdb");
+        System.out.println(iHelloService.helle("zdb"));
+
+    }
+
+}
---
+++ src/main/java/demo/dubbo/api/IHelloService.java
@@ -0,0 +1,8 @@
+package demo.dubbo.api;
+
+/**
+ * Created by DJ009828 on 2020/10/31 18:34
+ */
+public interface IHelloService {
+ String helle(String msg);
+}
